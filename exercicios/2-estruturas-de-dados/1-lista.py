19b039fa45b6c16bfe66870b52d3d7dfb16a7666
ptaishi <pedrotaishi@gmail.com>
2026-08-15 19:29:33 -0300

    Update 1-lista.py

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,14 @@
 # Crie uma lista apenas com elementos numéricos
-
+lista [1, 3, 5, 6]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+dog['mochi', 2, [2,4,5], true]
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+dog[5]
 # Crie um slice na lista para que imprima na tela os elementos de índice par
 
 # Remova da lista o último item
-
+dog.remove[true]
 # Insira na lista um novo item
-
+dog.insert[nickao]
 # Remova da lista um item específico
+dog.remove[2]
